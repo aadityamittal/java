3364da5c0b257eeccf8bf52f505e610fe6048112
Aaditya Mittal <66875383+aadityamittal@users.noreply.github.com>
2021-01-11 22:20:07 +0530

    Update Prob8.java

--- Prob8.java
+++ Prob8.java
@@ -1,41 +1,43 @@
 package probs;
 
-//Java program to print diamond pattern 
 import java.util.Scanner;
 
 class Prob8 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int i,j;
 		int n = sc.nextInt();
-		int space = n / 2, st = 1;
+		int k = n/2+1;
+		for(i=1; i<=n/2+1; i++)
+	    {   for(j = 1; j<k ;j++)
+	    	{System.out.print(" ");
+	    	}k--;
+	        for(j=1; j<=i; j++)
+	        {
+	           System.out.print(j);
+	        }
+	        for(j=i-1; j>=1; j--)
+	        {
+	        	System.out.print(j);
+	        }
 
-		for (int i = 1; i <= n; i++) {
+	        System.out.println();
+	    }
+		k = n/2;
+		for(i=n/2; i>=1; i--)
+	    {
+			for (int c = i; c <= n/2; c++)
+			     System.out.print(" ");
+	        for(j=1; j<=i; j++)
+	        {
+	            System.out.print(j);;
+	        }
+	        for(j=i-1; j>=1; j--)
+	        {
+	            System.out.print(j);
+	        }
 
-			for (int j = 1; j <= space; j++)
-				System.out.print(" ");
-
-			int count = st / 2 + 1;
-			for (int k = 1; k <= st; k++) {
-				System.out.print(count);
-				if (k <= st / 2)
-					count--;
-				else
-					count++;
-			}
-
-			System.out.println();
-			if (i <= n / 2) {
-
-				space = space - 1;
-				st = st + 2;
-			}
-
-			else {
-
-				space = space + 1;
-				st = st - 2;
-			}
-		}
+	        System.out.println();
+	    }
 	}
 }
